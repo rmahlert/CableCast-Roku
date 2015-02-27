60e8f26cfeba2c114728f65e44b961b7e327ceb9
rmahlert <rmahlert@gmail.com>
2015-02-26 19:46:38 -0500

    Updated SimpleImage.php

--- Cablecast-Roku.php
+++ Cablecast-Roku.php
@@ -1,30 +1,33 @@
 <?php
 
 // CableCastRoku 0.0.2
-// 6-23-14
+// 6-16-14
 // By Rob Mahlert
 //
 // Based off VODdisply.php by Ray Tiley and uses SimpleImage by Simon Jarvis
-// Thanks to Bryan Harley for the help and 'Escape ampersand' fix
+// 
+// Turn off output buffering
+ini_set('output_buffering', 'off');
+
+// Print to screen informing this will take a bit..
+// Remove after testing and using nightly cron
+// echo "Processing...<br/>";
+// ob_flush();
+// flush();
 
 // Call file to resize images
 include('SimpleImage.php');
 
-//Configure Sections
-$server = "http://studio.brimfield.tv/"; // URL to CableCast server
-$host = "http://test.brimfield.tv/"; // URL to webserver hosting files
-$imageurl = "http://test.brimfield.tv/roku/images/"; // URL to image files
+//Configure Section
+$server = "http://xxx/"; // URL to CableCast server
+$host = "http://xxx/"; // URL to webserver hosting files
+$imageurl = "http://xxx/images/"; // URL to image files
 $VODFile = "vod.xml"; // Name of XML file (needs to match in channel package files on Roku)
-$xmlpath = "/home/brimfieldtv/public_html/roku/xml/"; // path to xml file on server
-$imagepath = "/home/brimfieldtv/public_html/roku/images/"; // path to image directory
+$xmlpath = "/xxx/xml/"; // path to xml file on server
+$imagepath = "/xxx/images/"; // path to image directory
 date_default_timezone_set('America/New_York');
 
 // End Configure
-
-//Escape ampersand
-function xmlEscape($stringData) {
-return str_replace(array('&'), array('&amp;'), $stringData);
-} 
 
 // We need a count of the video files for the Roku 
 	$counter = 0;
@@ -40,7 +43,7 @@ $result = $client->AdvancedShowSearch(array(
     'searchString'         => '',
     'eventDate'           => date("Y-m-d") . "T00:00:00",
     'dateComparator'      => '<',
-    'restrictToCategoryID'  => 0,  
+    'restrictToCategoryID'  => 17,  
     'restrictToProducerID'   => 0, 
     'restrictToProjectID'    =>  0,
     'displayStreamingShowsOnly'   =>  1,
@@ -62,85 +65,89 @@ if(count($vods) == '0') {
     // Create Roku XML for for VOD
 $fh = fopen($xmlpath . $VODFile , 'w') or die("can't open file: " . $xmlpath . $VODFile);
 ob_end_flush();
+//Escape Characters
+function xmlEscape($stringData) {
+    return str_replace(array('&'), array('&amp;'), $stringData);
+}
 //Start XML Header
 $stringData = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>' . "\n";
 	   fwrite($fh, $stringData);
 	   $stringData = "<feed>" . "\n";
 	   fwrite($fh, $stringData);	   
-	   $stringData = "<resultLength>TEMPCOUNT</resultLength>" . "\n";
+	   $stringData = "<resultLength>TTLCOUNT</resultLength>" . "\n";
 	   fwrite($fh, $stringData);
 	   $stringData = "<endIndex>TTLCOUNT</endIndex>" . "\n";
 	   fwrite($fh, $stringData);
-		
+
 // Write XML for each VOD file	   
     foreach($vods as $vod) {	  
-	
+
 	// Add to counter
 	   $counter++;
-	   
+
     // Create thumbnail image URLs for Item
 		$url = $server . "Cablecast/ShowFiles/" . $vod->ShowID."/_thumbnail-1.png" ;
 		$imgHD = $imageurl . $vod->ShowID."_HD.png" ;
 		$imgSD = $imageurl . $vod->ShowID."_SD.png" ;
 	    $stringData = '<item sdImg="'.$imgSD . '"  hdImg="' . $imgHD . '">' . "/n";
 	    fwrite($fh, $stringData);	 
-	   
+
 	   // Write title of file
 	   $stringData = "<title>". $vod->Title . "</title>" . "\n";
 	   fwrite($fh, xmlEscape($stringData));
-	   
+
 	   // Show ID
 	   $stringData = "<contentId>". $vod->ShowID . "</contentId>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
 	   // Use Talk for default
 	   $stringData = "<contentType>Talk</contentType>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
 	   // Content Quality SD or HD (Defualt to SD for now)
 	   $stringData = "<contentQuality>SD</contentQuality>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
 	   // Stream type - Roku supports h.264
 	   $stringData = "<streamFormat>mp4</streamFormat>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
 	   // Start stream information
 	   $stringData = "<media>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
 	   // Stream Quality  SD or HD (Defualt to SD for now)
 	   $stringData = "<streamQuality>SD</streamQuality>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
 	   // Stream bitrate (In VOD settings on Cablecast) Can it be pulled from VOD?
 	   $stringData = "<streamBitrate>2000</streamBitrate>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
 	   // URL to CableCast VOD stream
 	   $stringData = "<streamUrl>" . $vod->StreamingFileURL . "</streamUrl>" . "\n";
-	   fwrite($fh, xmlEscape($stringData));
-	   
+	   fwrite($fh, $stringData);
+
 	   // End Media Tag section
 	   $stringData = "</media>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
 	   // Description of the video
-	   $stringData = "<synopsis> . $vod->Comments . </synopsis>" . "\n";
+	   $stringData = "<synopsis>" . $vod->Comments ."</synopsis>" . "\n";
 	   fwrite($fh, xmlEscape($stringData));
-	   
-	   // Needs to match in channel package files on Roku - Categories.brs
-	   $stringData = "<genres>Meetings</genres>" . "\n";
+
+	   // Release date
+	   $stringData = "<releasedate>".date('n/j/Y', strtotime($vod->EventDate))."</releasedate>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
 	   // Length of video in seconds	   
 	   $stringData = "<runtime>". $vod->TotalSeconds . "</runtime>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
 	   // End Item tag for this video
 	   $stringData = "</item>" . "\n";
 	   fwrite($fh, $stringData);
-	   
+
  // End writing VOD
  
  // Load thumbnail and resize for Roku HD and SD image files
@@ -152,7 +159,7 @@ $stringData = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>' . "\n";
 	// Retrieve image off CableCast and resize
 		$image = new SimpleImage();
 		
-  // HD image exist?
+	// HD image exist?
 		if (file_exists($imgHD)) {
   } else {
   		$image->load($url);
@@ -164,9 +171,14 @@ $stringData = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>' . "\n";
 		$image->resize(214,144);
 		$image->save($imgSD);
 	}		
-	
+
 	// Back to next video file
 
+	// For  little feedback while testing - note: will display when done processing ALL 
+	// Remove after testing and using nightly cron
+ 	echo "Added VOD File:" . $vod->ShowID . "<br/>";
+	ob_flush();
+	flush();
 
     }
 
